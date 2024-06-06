97f95f2e1598788526f66cb2c33eb7b8dcbefacb
Lufuno Vele <145297497+Elelwani17@users.noreply.github.com>
2024-06-06 16:58:24 +0200

    Create WeatherForecast.js

---
+++ src/WeatherForecast.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [loaded, setloaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setloaded(false);
+  }, [props.coordinates]);
+
+  function getForecast(response) {
+    setForecast(response.data.daily);
+    setloaded(true);
+  }
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay forecastData={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    console.log(props.coordinates);
+    let lon = props.coordinates.longitude;
+    let lat = props.coordinates.latitude;
+    let apiKey = "48d9c02baa93fa8d733783cd33ot621f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(getForecast);
+  }
+  return null;
+}
