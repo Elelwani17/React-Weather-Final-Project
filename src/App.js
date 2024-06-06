337f2f88353d77d5a6b8469755e0598a54a05403
Lufuno Vele <145297497+Elelwani17@users.noreply.github.com>
2024-06-06 16:03:48 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import WeatherSearch from "./WeatherSearch";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Get a Weather a Forecast</h1>
+      <WeatherSearch />
+      <br />
+      <br />
+      <div>
+        <a href="https://github.com/Elelwani17/SheCodes-React-Weather-Search-Engine">Github</a>
+      </div>
+    </div>
+  );
+}
